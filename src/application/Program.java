3808af61bb93819d366a6c3d688aaaf1e71d9607
Pedro Gomes <you@example.com>
2021-07-31 13:00:58 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
